00775314875959dd1fbb7f92f0e2247ab28891cd
Hideaki Tai <hideaki.tai@gmail.com>
2021-04-07 13:38:51 +0900

    update example

--- examples/base_ringbuffer/base_ringbuffer.ino
+++ examples/base_ringbuffer/base_ringbuffer.ino
@@ -1,6 +1,6 @@
 #include <ArxContainer.h>
 
-ArxRingBuffer<uint8_t, 4> buffer;
+ArxRingBuffer<int8_t, 4> buffer;
 
 void setup() {
     Serial.begin(115200);
@@ -15,14 +15,18 @@ void setup() {
     buffer.push(4);
 
     Serial.println("for loop (indexed) should be 1, 2, 3, 4");
-    for (size_t i = 0; i < buffer.size(); ++i)
+    for (size_t i = 0; i < buffer.size(); ++i) {
+        Serial.print("index ");
+        Serial.print(i);
+        Serial.print(": ");
         Serial.println(buffer[i]);
+    }
 
     Serial.println();
     buffer.pop();
 
     Serial.println("for loop (ranged) should be 2, 3, 4");
-    for (auto& b : buffer)
+    for (const auto& b : buffer)
         Serial.println(b);
 
     Serial.println();
@@ -33,8 +37,17 @@ void setup() {
         Serial.println(*it);
 
     Serial.println();
-    Serial.println("ringbuffer test finished");
+    Serial.println("===========================================================");
 }
 
 void loop() {
+    static int count = 5;
+    delay(500);
+
+    buffer.push(count++);
+    for (const auto& b : buffer) {
+        Serial.print(b);
+        Serial.print(", ");
+    }
+    Serial.println();
 }
